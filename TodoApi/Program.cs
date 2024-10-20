288dbe258679439de047cc0edabc40586b77a64d
AdityaSeth777 <adityaseth.cse@gmail.com>
2024-10-20 13:33:17 +0530

    before https://github.com/microsoft/workshop-library/blob/main/full/intro-minapi/README.md#create-new-items

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,18 +1,32 @@
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<TodoDb>(options => options.UseInMemoryDatabase("items"));
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Description = "Keep track of your tasks", Version = "v1" });
 });
 var app = builder.Build();
-
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
 });
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/todo", () => new { Item = "Water plants", Complete = "false" });
+app.MapGet("/todos", async (TodoDb db) => await db.Todos.ToListAsync());
 app.Run();
+
+class TodoItem
+{
+    public int Id { get; set; }
+    public string? Item { get; set; }
+    public bool IsComplete { get; set; }
+}
+
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions options) : base(options) { }
+    public DbSet<TodoItem> Todos { get; set; }
+}
