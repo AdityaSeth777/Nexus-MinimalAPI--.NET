4dabb10c689f7f82e654fc770431218957097de4
AdityaSeth777 <adityaseth.cse@gmail.com>
2024-10-20 13:26:16 +0530

    swagger added - step 2

--- TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1498dd13eeb33cc1ca0dfac587544579a8ade0e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
