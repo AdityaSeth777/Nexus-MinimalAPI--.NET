f95c083b65ef8924412ea3e7b52ca2475b63cc90
AdityaSeth777 <adityaseth.cse@gmail.com>
2024-10-20 14:03:05 +0530

    ALL DONE EXCEPT DELETE

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -15,7 +15,29 @@ app.UseSwaggerUI(c =>
    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
 });
 app.MapGet("/", () => "Hello World!");
+
 app.MapGet("/todos", async (TodoDb db) => await db.Todos.ToListAsync());
+app.MapPost("/todos", async (TodoDb db, TodoItem todo) =>
+{
+    await db.Todos.AddAsync(todo);
+    await db.SaveChangesAsync();
+    return Results.Created($"/todo/{todo.Id}", todo);
+});
+app.MapGet("/todos/{id}", async (TodoDb db, int id) => await db.Todos.FindAsync(id));
+app.MapPut("/todos/{id}", async ( TodoDb db, TodoItem updateTodo ,int id) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+    
+    if (todo is null) return Results.NotFound();
+    
+    todo.Item = updateTodo.Item;
+    todo.IsComplete = updateTodo.IsComplete;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
 app.Run();
 
 class TodoItem
--- TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dabb10c689f7f82e654fc770431218957097de4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+288dbe258679439de047cc0edabc40586b77a64d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
